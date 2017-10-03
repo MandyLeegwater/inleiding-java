018e233609cc11cf64e8059f8d73cb318dd63e3f
m-lee <m-leegwater@hotmail.com>
2017-10-03 13:57:56 +0200

    button toegevoegd

--- src/h11/praktijkopdracht11a.java
+++ src/h11/praktijkopdracht11a.java
@@ -7,39 +7,55 @@ import java.awt.event.ActionListener;
 
 public class praktijkopdracht11a extends Applet {
     int tafelinput;
-    int i,x,y;
+    int i, x, y;
     TextField vak1;
+    Button okKnop;
 
-    public void init(){
-        setSize(300,500);
+    public void init() {
         tafelinput = 0;
         vak1 = new TextField();
-        vak1.setSize(300,500);
+        vak1.setSize(300, 500);
         Listen1 l1 = new Listen1();
         vak1.addActionListener(l1);
         add(vak1);
+        okKnop = new Button("Ok");
+        OkListener Ok = new OkListener();
+        okKnop.addActionListener(Ok);
+        add(okKnop);
+
     }
 
-    public void paint(Graphics g){
+    public void paint(Graphics g) {
         x = 60;
         y = 60;
 
-        if (tafelinput>0){
-            for (i = 0; i<10;){
-                y = y+20;
+        if (tafelinput > 0) {
+            for (i = 0; i < 10; ) {
+                y = y + 20;
                 i++;
-                g.drawString(i+"x"+tafelinput+"="+tafelinput*i,x,y);
+                g.drawString(i + "x" + tafelinput + "=" + tafelinput * i, x, y);
             }
         }
-
-        vak1.setSize(50,50);
+        
 
     }
-    class Listen1 implements ActionListener{
+
+    class Listen1 implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             tafelinput = (Integer.parseInt(vak1.getText()));
             vak1.setText("");
             repaint();
         }
     }
+    public class OkListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+
+            tafelinput = Integer.parseInt(vak1.getText());
+            vak1.setText("");
+            repaint();
+
+
+        }
+    }
 }
+
