feea8997122c105056b93466d903b9aec6f47603
m-lee <m-leegwater@hotmail.com>
2017-10-13 14:01:54 +0200

    Challenge Fruitautomaat Staafdiagram Wereldtijdem

---
+++ src/Challenge/Staafdiagram.java
@@ -0,0 +1,83 @@
+package Challenge;
+
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Staafdiagram extends Applet {
+    private Color Kleur;
+    private TextField TekstvakValerie;
+    private TextField TekstvakJeroen;
+    private TextField TekstvakHans;
+    private int Valerie, Hans, Jeroen;
+
+    public void init() {
+        setSize(500, 500);
+
+        Label labelValerie = new Label("Valerie");
+        add(labelValerie);
+        TekstvakValerie = new TextField("", 7);
+        add(TekstvakValerie);
+
+        Label labelJeroen = new Label("Jeroen");
+        add(labelJeroen);
+        TekstvakJeroen = new TextField("", 7);
+        add(TekstvakJeroen);
+
+        Label labelHans = new Label("Hans");
+        add(labelHans);
+        TekstvakHans = new TextField("", 7);
+        add(TekstvakHans);
+
+        Button knop = new Button("Toon");
+        knopListener knopListener = new knopListener();
+        knop.addActionListener(knopListener);
+        add(knop);
+
+        Kleur = Color.red;
+        Valerie = 0;
+        Hans = 0;
+        Jeroen = 0;
+    }
+
+    public void paint(Graphics g) {
+        g.drawLine(45, 80, 45, 340);
+        g.drawLine(45, 340, 300, 340);
+        g.drawString("0", 25, 340);
+        g.drawString("20", 20, 300);
+        g.drawString("40", 20, 260);
+        g.drawString("60", 20, 220);
+        g.drawString("80", 20, 180);
+        g.drawString("100", 15, 140);
+        g.drawString("120", 15, 100);
+        g.drawString("Valerie", 60, 360);
+        g.drawString("Jeroen",100,360);
+        g.drawString("Hans",160,360);
+
+        g.setColor(Kleur);
+        g.fillRect(60, 340 - Valerie, 30, Valerie);
+        g.fillRect(110,340 - Jeroen,30,Jeroen );
+        g.fillRect(160,340 - Hans,30, Hans);
+    }
+
+    class knopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String getal1 = TekstvakValerie.getText();
+            Valerie = Integer.parseInt(getal1);
+            Valerie = Valerie * 2;
+            repaint();
+
+            String getal2 = TekstvakJeroen.getText();
+            Jeroen = Integer.parseInt(getal2);
+            Jeroen = Jeroen * 2;
+            repaint();
+
+            String getal3 = TekstvakHans.getText();
+            Hans = Integer.parseInt(getal3);
+            Hans = Hans * 2;
+            repaint();
+        }
+    }
+}
---
+++ src/Challenge/Wereldtijden.java
@@ -0,0 +1,65 @@
+package Challenge;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Wereldtijden extends Applet {
+    private TextField vak1;
+    private int tijd, sydney, london, washington, tokyo;
+    private String error;
+
+    public void init() {
+        vak1 = new TextField("", 15);
+        add(vak1);
+        tijd = 0;
+        Button knop = new Button("Toon Tijden");
+        knopListener knopListener = new knopListener();
+        knop.addActionListener(knopListener);
+        add(knop);
+    }
+
+    public void paint(Graphics g) {
+
+        int x = 10;
+        int y = 50;
+        if (tijd < 24) {
+            g.drawString("Sydney: " + sydney + ":00", x, y);
+            g.drawString("London: " + london + ":00", x, y+20);
+            g.drawString("Washington: " + washington + ":00", x, y+40);
+            g.drawString("Tokyo: " + tokyo + ":00", x, y+60);
+        } else {
+            g.drawString(error, x, y);
+        }
+    }
+
+    class knopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            tijd = Integer.parseInt(vak1.getText());
+            if (tijd < 24) {
+                sydney = tijd + 9;
+                if (sydney > 23) {
+                    sydney = sydney - 24;
+                }
+                tokyo = tijd + 7;
+                if (tokyo > 23) {
+                    tokyo = tokyo - 24;
+                }
+                london = tijd - 1;
+                if (london < 0) {
+                    london = london + 24;
+                }
+                washington = tijd - 6;
+                if (washington < 0) {
+                    washington = washington + 24;
+                }
+            }
+            else {
+                error ="Voer een geldig uur in";
+            }
+            repaint();
+        }
+    }
+}
+
